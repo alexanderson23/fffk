f8a94b28ab3c49ecbd70c1cd4e671a9b50ddccc6
alex <sasha.2302@bk.ru>
2020-07-30 22:42:28 +0300

    End

--- src/js/script.js
+++ src/js/script.js
@@ -101,5 +101,39 @@ $(document).ready(function(){
     validateForms('#order form')
     validateForms('#consultation')
 
-    $('input[name=phone]').mask("+7 (999) 999-9999");
+    $('input[name=phone]').mask("+7 (999) 999-99- 99");
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+    // Smooth scroll and pageup
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600){
+            $('.pageup').fadeIn();
+        }else{
+            $('.pageup').fadeOut();
+        }
+    });
+    
+    $("a[href^=#up]").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+    new WOW().init();
 });
